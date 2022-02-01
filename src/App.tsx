fe20ef1b0835e5c71dd60a9d5f8be8dcc4714a5e
Mereuta Sebastian <mereutasebastian@Bytexs-MacBook-Pro.local>
2022-02-01 16:30:13 +0200

    basic canvas drawings

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,42 @@ import logo from './logo.svg';
 import './App.css';
 import ResLayout from './components/RestaurantLayout/ResLayout';
 
-const App: React.FC = ()=> {
+
+
+const App: React.FC = () => {
+  const draw = (ctx: any): void => {
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    ctx.fillStyle = '#000000';
+    ctx.fillRect(20, 0, 30, 60);
+
+    ctx.fillStyle = '#000000';
+    ctx.fillRect(80, 0, 30, 60);
+
+    ctx.fillStyle = '#000000';
+    ctx.fillRect(140, 0, 30, 60);
+
+    ctx.fillStyle = '#000000';
+    ctx.fillRect(90, 100, 30, 30);
+
+    ctx.fillStyle = '#000000';
+    ctx.fillRect(150, 100, 30, 30);
+
+    ctx.fillStyle = 'rgb(200,200,200)';
+    ctx.fillRect(210, 0, 90, 90);
+
+    ctx.fillStyle = '#000000';
+    ctx.beginPath();
+    ctx.arc(40, 110, 22, 0, 2 * Math.PI);
+    ctx.fill();
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <ResLayout/>
+      {/* [to do] add style to header*/}
+      {/* [to do] header should contain: date, hour, table number, reserve button, firebase - login component*/}
+      <header>some Header data</header>
+      <ResLayout draw={draw} />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/RestaurantLayout/ResLayout.tsx
+++ src/components/RestaurantLayout/ResLayout.tsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
-const ResLayout: React.FC = () => {
-  return <div>In Restaurant Layout Component</div>;
+//[to do] refactor when will decide on libraries for canvas;
+const canvasStyle = {
+  backgroundColor: '#eeee',
+  borderRadius: '5px',
+  border: '1px solid gray',
+};
+
+const ResLayout: React.FC<{ draw: Function }> = (props) => {
+  const { draw } = props;
+  const canvasRef = useRef(null);
+
+  useEffect(
+    () => {
+      const canvas: any = canvasRef.current;
+      const context = canvas.getContext('2d');
+      draw(context);
+    },
+    [ draw ],
+  );
+
+  return (
+    <div>
+      <p className="">In Restaurant Layout Component</p>
+
+      <canvas ref={canvasRef} style={canvasStyle} />
+    </div>
+  );
 };
 
 export default ResLayout;
