dafc086b8613295ed032b0d609a7b59548d5bcd8
Mereuta Sebastian <mereutasebastian@Bytexs-MacBook-Pro.local>
2022-02-07 12:07:45 +0200

    changes+gitignore firebaseAppConfig for security reasons

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,38 @@ import './App.css';
 import ResLayout from './components/RestaurantLayout/ResLayout';
 import { Rectangle } from './utils/classes/Rectangle';
 import { db, auth } from './utils/firebase';
-import { collectionGroup, collection, doc, getDoc, setDoc, onSnapshot, getDocs } from 'firebase/firestore';
+import { collectionGroup, addDoc,doc, getDoc, setDoc, onSnapshot, getDocs, query, collection } from 'firebase/firestore';
 
 //questions:
 //how to handle posts and get data from collection; in app? or separate and import
 //uuid for document id?
 //here doc(db,'tables',"H0CsyvTqhFoOt65ks3G7"); it's pretty useless to pass the document id mannunally.
 //how to get it whithout having it to pass it manually
+//found addDoc, care adauga un document in colectie, cu auto-id. why use setDoc(collectionReference,documentId)?
+//firebase Types not working? line 33; daca pun const theTables:CollectionReference<DocumentData> da eroare. trebuie facut import separat de types?
+
 
 const App: React.FC = () => {
   const rect = new Rectangle(300, 100, 10, 10, '#000');
   const rect2 = new Rectangle(250, 180, 10, 10, '#000');
   const [ restData, setRestData ]: any = useState<object[]>();
+  const [ tables, setTables ] = useState<any>([]);
 
-  const tables = collection(db, 'tables');
+  //next line has to be used in useeffect, but it's verbose. getdoc-> query->collection->.then(res)->res.doc.map(doc)->doc.data sa imi afiseze documentele dintr-o collectie..
+  // !tables.length && getDocs(query(collection(db, 'tables'))).then((res) => setTables(res.docs.map((doc) => doc.data())));
+  // console.log(tables);
+  
 
   // const tablesQuerySnapshot = async () => await getDocs(tables)
   // tablesQuerySnapshot().then((res)=>{
   //   res.docs.forEach((doc)=>console.log(doc.id,doc.data()))
   // }
   // );
+
+  const theTables = collection(db,"tables")
+
+   let addTheData = ()=>addDoc(theTables,{smth:"dada"})
+   
 
   const draw = (ctx: any) => {
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
@@ -56,7 +68,7 @@ const App: React.FC = () => {
 
   const docRef = doc(db, 'tables', 'H0CsyvTqhFoOt65ks3G7');
   const unsub = onSnapshot(docRef, (doc) => {
-    console.log(doc.data());
+    // console.log(doc.data());
   });
   // const data = onSnapshot(tables, (doc) => {
   //   doc.docs.map((item) => console.log(item.data()));
@@ -74,10 +86,12 @@ const App: React.FC = () => {
   // tot timpul cand adaug ceva, trebuie sa fac un reference la collection/documentId sa pun ceva.
   //nu ma pot folosi de un closure sa nu trebuiasca scriu tot timpul acelasi lucru?
   function addToTablesCollection(item: object){
-    return async function postData(){
-      await setDoc(doc(db, 'tables', 't3'), item);
-    };
+    async function postData(){
+      return await setDoc(doc(db, 'tables', 't5'), item);
+    }
+    return postData();
   }
+
   // const postData = async () => {
   //   await setDoc(doc(db, 'tables', 't2'), {
   //     maxSeats: 2,
@@ -89,14 +103,17 @@ const App: React.FC = () => {
   // let fn1=addToTablesCollection({});
   // console.log(fn1());
 
-  const handlePostData = () => {
-    let data = addToTablesCollection({
-      maxSeats: 6,
-      reservastion: new Date(),
-      reservationName: 'alexya',
-      tableNr: 3,
-    });
-    data();
+  const handlePostData = ():void => {
+    try {
+      addToTablesCollection({
+        maxSeats: 6,
+        reservastion: new Date(),
+        reservationName: 'alexya',
+        tableNr: 3,
+      });
+    } catch (e:any) {
+      console.log(e.message);
+    }
   };
 
   useEffect(() => {
@@ -109,7 +126,7 @@ const App: React.FC = () => {
       {/* [to do] header should contain: date, hour, table number, reserve button, firebase - login component*/}
       <header>some Header data</header>
       <ResLayout draw={draw} rect={rect} />
-      <button onClick={handlePostData}>add some data</button>
+      <button onClick={addTheData}>add some data</button>
       <div />
     </div>
   );
--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,32 +1,16 @@
 import { initializeApp } from 'firebase/app';
+import {firebaseAppConfig} from './firebaseAppConfig'
 import { getFirestore } from 'firebase/firestore';
 import { getDatabase } from 'firebase/database';
 import { getAuth } from 'firebase/auth';
 import { getStorage, ref } from 'firebase/storage';
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDoc,  } from "firebase/firestore";
 
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-interface FirebaseConfig {
-  apiKey: string;
-  authDomain: string;
-  projectId: string;
-  storageBucket: string;
-  messagingSenderId: string;
-  appId: string;
-}
-
-const firebaseConfig: FirebaseConfig = {
-  apiKey: 'AIzaSyDg9intOFGHX851R78vhXqk3Akjndz8i1Q',
-  authDomain: 'restaurantapp-47f5a.firebaseapp.com',
-  projectId: 'restaurantapp-47f5a',
-  storageBucket: 'restaurantapp-47f5a.appspot.com',
-  messagingSenderId: '249892659350',
-  appId: '1:249892659350:web:f51fc1ae42d88437a862b7',
-};
+const firebaseConfig = firebaseAppConfig;
 
 // Initialize Firebase
 const app: any = initializeApp(firebaseConfig);
