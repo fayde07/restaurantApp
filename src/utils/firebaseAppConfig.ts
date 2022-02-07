a3592bc285fc268046818856a19e8a7d2f051b79
Mereuta Sebastian <mereutasebastian@Bytexs-MacBook-Pro.local>
2022-02-07 14:57:24 +0200

    changes

---
+++ src/utils/firebaseAppConfig.ts
@@ -0,0 +1,16 @@
+interface FirebaseConfig {
+  apiKey: string;
+  authDomain: string;
+  projectId: string;
+  storageBucket: string;
+  messagingSenderId: string;
+  appId: string;
+}
+export const firebaseAppConfig: FirebaseConfig = {
+  apiKey: 'AIzaSyDg9intOFGHX851R78vhXqk3Akjndz8i1Q',
+  authDomain: 'restaurantapp-47f5a.firebaseapp.com',
+  projectId: 'restaurantapp-47f5a',
+  storageBucket: 'restaurantapp-47f5a.appspot.com',
+  messagingSenderId: '249892659350',
+  appId: '1:249892659350:web:f51fc1ae42d88437a862b7',
+};
